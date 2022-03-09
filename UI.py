621613a58df354255387ea6337407712455d7374
2021dfff <1374490218@qq.com>
2022-03-09 21:25:41 +0800

    UI update

--- UI.py
+++ UI.py
@@ -1,22 +1,51 @@
 import sys
 import os
 import shutil
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
 class MainForm(QWidget):
     def __init__(self, name = 'MainForm'):
         super(MainForm,self).__init__()
-        self.setWindowTitle("选择文件")
         self.cwd = os.getcwd() # 获取当前程序文件位置
-        self.resize(300,200)  
+        self.setWindowTitle("Markdown Format Converter")
+        self.setWindowIcon( QIcon (self.cwd + '\\icon\\tiger.png'))
+        self.resize(400,100)  
         # btn 1
         self.btn_chooseFile = QPushButton(self)  
+        self.btn_chooseFile.setFont(QtGui.QFont('Comic Sans MS', 14))
+        self.btn_chooseFile.setStyleSheet(
+                                 "QPushButton{color:rgb(0,0,0)}" #按键前景色
+                                 "QPushButton{background-color:rgb(202, 195, 187)}"  #按键背景色
+                                 "QPushButton:hover{color:rgb(240, 235, 229)}" #光标移动到上面后的前景色
+                                 "QPushButton{border-radius:6px}"  #圆角半径
+                                 "QPushButton:pressed{background-color:rgb(180,180,180);border: None;}" #按下时的样式
+                                 )
+        # self.btn_chooseFile.setFixedSize(400,50)
+        self.btn_chooseFile.move(100, 100)
+        icon_img = QtGui.QIcon(self.cwd + '\\icon\\md.png')
+        self.btn_chooseFile.setIcon(icon_img)
         self.btn_chooseFile.setObjectName("btn_chooseFile")  
-        self.btn_chooseFile.setText("选取文件并保存至source目录下")
+        self.btn_chooseFile.setText("Choose a Markdown File")
         # btn 2
         self.btn_executeFile = QPushButton(self)  
+        self.btn_executeFile.setFont(QtGui.QFont('Comic Sans MS', 14))
+        self.btn_executeFile.setStyleSheet(
+                                 "QPushButton{color:rgb(0,0,0)}" #按键前景色
+                                 "QPushButton{background-color:rgb(193, 203, 215)}"  #按键背景色
+                                 "QPushButton:hover{color:rgb(240, 235, 229)}" #光标移动到上面后的前景色
+                                 "QPushButton{border-radius:6px}"  #圆角半径
+                                 "QPushButton:pressed{background-color:rgb(180,180,180);border: None;}" #按下时的样式
+                                 )
+        # self.btn_executeFile.resize(self.btn_chooseFile.sizeHint()) 
+        self.btn_executeFile.move(100, 100)
+        icon_img = QtGui.QIcon(self.cwd + '\\icon\\100.png')
+        self.btn_executeFile.setIcon(icon_img)
         self.btn_executeFile.setObjectName("btn_executeFile")  
-        self.btn_executeFile.setText("执行文件转换")
+        self.btn_executeFile.setText("Execute File Conversion")
+
 
         # 设置布局
         layout = QVBoxLayout()
@@ -33,8 +62,6 @@ class MainForm(QWidget):
                                     "选取文件",  
                                     self.cwd, # 起始路径 
                                     "Markdown Files (*.md);;PDF Files(*.pdf);;All Files (*)")   # 设置文件扩展名过滤,用双分号间隔
-        
-        shutil.copyfile(fileName_choose, self.cwd+ '\\source')
 
         if fileName_choose == "":
             print("\n取消选择")
@@ -44,28 +71,12 @@ class MainForm(QWidget):
         print(fileName_choose)
         #print(self.cwd+ '\\history')
         print("文件筛选器类型: ",filetype)
+
+        shutil.copy(fileName_choose, self.cwd+ '\\source')
         
-        #path = r"E:/Desktop/秃头文件/●应用软件课程设计/Individual Assignment/wx_press_guide/source"
-
-       
-
-
     def slot_btn_executeFile(self):
 
         os.system('python main.py')
-
-        #fileName_choose, filetype = QFileDialog.getSaveFileName(self,  
-        #                            "文件保存",  
-        #                            self.cwd, # 起始路径 
-        #                            "All Files (*);;Markdown Files (*.md)")  # 设置文件扩展名过滤,用双分号间隔
-
-        #if fileName_choose == "":
-        #    print("\n取消选择")
-        #    return
-
-        #print("\n你选择要保存的文件为:")
-        #print(fileName_choose)
-        #print("文件筛选器类型: ",filetype)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
